fd8ae9fa8d3a3cd4a5a010a92be05d5185832a0d
SongHeeJae <gmlwo308@gmail.com>
2021-02-09 22:01:26 +0900

    feat: 메인 비디오 전체 화면 기능 구현

--- src/components/views/VideoMeetingPage/Video/Video.js
+++ src/components/views/VideoMeetingPage/Video/Video.js
@@ -1,15 +1,15 @@
 import React, { useRef, useEffect, useState } from "react";
-
+import "./Video.css";
 const Video = (props) => {
   const videoRef = useRef();
-
   useEffect(() => {
     if (props.stream) {
       videoRef.current.srcObject = props.stream;
     }
   }, [props.stream]);
 
-  const onClick = () => {
+  const onClick = (e) => {
+    e.preventDefault();
     if (!props.onClick) return;
     props.onClick(videoRef.current.srcObject, props.username);
   };
@@ -18,12 +18,14 @@ const Video = (props) => {
     <>
       <div>
         <video
+          id="video"
           style={{ width: "100%", height: "100%" }}
           autoPlay
           playsInline
           ref={videoRef}
           onClick={onClick}
           muted={props.muted}
+          controls={props.onClick ? false : true}
         />
       </div>
       <div>{props.username}</div>
